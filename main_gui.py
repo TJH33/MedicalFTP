7ee284e38af02cb82dc10c1d375c2939c45af891
= <=>
2022-08-18 16:27:23 +0100

    Working on button events

--- main_gui.py
+++ main_gui.py
@@ -1,11 +1,13 @@
 # This is the python script that will look after the main GUI, alongside the validation of CSV files as they are downloaded
 # Author: Thomas H2
 
+#
 
 from tkinter import *
 from tkinter.ttk import *
 from tkcalendar import Calendar
 from datetime import datetime as dt
+import scheduler as sc
 
 # this rolls the main window over the screen
 
@@ -35,15 +37,21 @@ def main():
 
     # styling and organising buttons
 
-    btn_schedule_eday = Button(window, text='Schedule Every Day', command=None).pack(anchor='s')
+    btn_schedule_eday = Button(window, text='Schedule Every Day', command=lambda: schedule_event(given_date=cal.selection_get(), every_day_switch=True)).pack(anchor='s')
     btn_schedule = Button(window, text='Schedule at Calendar Date', command=None)
     btn_CSV = Button(window, text='Get CSV Data For Calendar Date', command=None)
     btn_schedule.pack(side=LEFT, anchor='se')
     btn_CSV.pack(side=RIGHT, anchor='sw')
 
-    
 
     window.mainloop()
+
+# procedure which goes to perform the schedling task
+
+def schedule_event(given_date, every_day_switch):
+    print(given_date)
+
+
 
 if __name__ == '__main__':
     main()
--- scheduler.py
+++ scheduler.py
@@ -2,126 +2,35 @@
 # Author: Thomas H2
 # References are in line the script file
 
-
-#Modules imported for GUI 
-
-from time import time
-from tkinter import *
-from tkinter import ttk
-from tkcalendar import Calendar
-
 #Modules imported for scheduling tasks
 
-from apscheduler.schedulers.background import BackgroundScheduler 
+from apscheduler.schedulers.blocking import BlockingScheduler 
 from datetime import datetime as dt
 
 # Modules imported for ftp connection
 
 import ftplib as ftp
 
-#sets the window's properties
-
-window = Tk()
-
-def main():
-
-    # strings for spinbox
-
-    hour_string = StringVar()
-    min_string = StringVar()
-    last_value_sec = ''
-    last_value = ''
-
-    # window geometry
-
-    window.geometry('600x400')
-    window.resizable(False, False)
-    window.title("Scheduler")
-
-    fone = Frame(window)
-    ftwo = Frame(window)
-
-    # Add Calendar
-
-    cal = Calendar(fone, selectmode = 'day',
-               year = dt.now().year, month = dt.now().month,
-               day = dt.now().day)
-
-    
-    # reference = https://pythonguides.com/create-date-time-picker-using-python-tkinter/
-
-
-    # spinboxes which look after the hours, minutes, seconds counter
-    
-    min_sb = Spinbox(
-        ftwo,
-        from_=0,
-        to=23,
-        wrap=True,
-        textvariable=hour_string,
-        width=2,
-        state="readonly",
-        justify=CENTER
-    )
-
-    hour_sb = Spinbox(
-        ftwo,
-        from_= 0,
-        to = 59,
-        wrap=True,
-        textvariable=min_string,
-        width=2,
-        justify=CENTER
-    )
-
-    sec_sb = Spinbox(
-       ftwo,
-       from_ = 0,
-       to=59,
-       wrap=True,
-       textvariable=hour_sb,
-       width=2,
-       justify=CENTER
-    )
-
-    sec = Spinbox(
-        ftwo,
-        from_= 0,
-        to=59,
-        wrap=True,
-        textvariable=sec_sb,
-        width=2,
-        justify=CENTER
-    )
-
-    cal.pack(pady = 20)
-    min_sb.pack(side=LEFT, fill=X, expand=True)
-    hour_sb.pack(side=LEFT, fill=X, expand=True)
-    sec_sb.pack(side=LEFT, fill=X, expand=True)
-    sec.pack(side=LEFT, fill=X, expand=True)
-
-    fone.pack()
-    ftwo.pack()
-
-    window.mainloop()
-
 #function will start the scheduling at a given date
 
-def schedule_event(given_time):
+def schedule_event(given_time, is_e_day):
+    scheduler = BlockingScheduler()
+    scheduler.start()
+    # determines if there is already another job
+
+    # starts the jobs
     present = dt.now()
-    if given_time <= present:
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(ftp_job, args=[given_time], trigger='cron', day_of_week=given_time.weekday())
+    if given_time <= present and is_e_day:
+        scheduler.add_job(ftp_job, trigger='cron', hour='0', day_of_week='*', id='ftp_job')
         return True
-    else:
-        return False
+    elif given_time <= present and not is_e_day:
+        scheduler.add_job(ftp_job, trigger='cron', hour=str(given_time.hour), day=str(given_time.day), year=str(given_time.year))
 
 
 def ftp_job():
     # get the current date-time
 
     current_date = dt.now()
-
 
     # logging in using anonymous creds
 
@@ -132,7 +41,3 @@ def ftp_job():
     ftp.dir()
 
     return True
-
-
-if __name__ == '__main__':
-    main()
