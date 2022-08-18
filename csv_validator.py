1866648677216a7dc44b4b760299ecfafa06b13b
ojmartin <35339148+Kynemo@users.noreply.github.com>
2022-08-18 23:45:58 +0100

    Fixed validation

--- csv_validator.py
+++ csv_validator.py
@@ -5,8 +5,41 @@ import re
 def validate_csv(file):
     test_criteria = []
 
-    with open(csv.reader) as csv_file:
-        file = csv_file.readlines()
+    with open(file, 'r+') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        header = next(csv_reader)
+
+        if not validate_header(header):
+            print('Invalid header')
+
+        batch_ids = []
+        for line in csv_reader:
+            if id := line[0] not in batch_ids:
+                batch_ids.append(id)
+            # If duplicate raise an error
+            else:
+                test_criteria.append('Duplicate batch id')
+
+            if not validate_timestamp(line[1]):
+                print(line[1])
+                test_criteria.append('Invalid timestamp')
+
+            readings = line[2:]
+            total_readings = 0
+            for reading in readings:
+                total_readings += 1
+                if not validate_float(reading):
+                    print(reading)
+                    test_criteria.append('Invalid reading')
+                    break
+
+            if total_readings != 10:
+                test_criteria.append("Invalid number of readings")
+
+        return test_criteria
+
+
 
         if not validate_header(get_header(file)):
             # If invalid header, patch it on the fly
@@ -16,47 +49,19 @@ def validate_csv(file):
             csv_file.writelines(file)
         batch_ids = []
 
-        for line in file[1:]:
-            # Check if batch id is a duplicate
-            if id := line[0] not in batch_ids:
-                batch_ids.append(id)
-            # If duplicate raise an error
-            else:
-                test_criteria.append('Duplicate batch id')
-
-            if not validate_timestamp(line[1]):
-                test_criteria.append('Invalid timestamp')
-
-            readings = line[2:]
-            total_readings = 0
-            for reading in readings:
-                total_readings += 1
-                if not validate_float(reading):
-                    test_criteria.append('Invalid reading')
-                    break
-
-            if total_readings != 10:
-                test_criteria.append("Invalid number of readings")
-
-    return test_criteria
-
 
 def validate_float(num):
-    float_regex = "(?:[0-8][0-9]{2}|900))"
-    return re.match(float_regex, num)
+    float_regex = "(?:[0-8]\.[0-9]{3}|9\.(?:[0-8][0-9]{2}|900))"
+    return re.search(float_regex, num)
 
 
 def validate_timestamp(time):
-    time_regex = '\"(?:[0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]\"'
-    return re.match(time_regex, time)
+    time_regex = '(?:[0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]'
+    return re.search(time_regex, time)
 
 
 def validate_header(header):
     valid_header = ["batch_id", "timestamp", "reading1", "reading2", "reading3", "reading4", "reading5", "reading6",
                     "reading7", "reading8", "reading9", "reading10"]
     return header == valid_header
-
-
-def get_header(file):
-    return file[0]
 
